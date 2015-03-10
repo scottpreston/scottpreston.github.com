395be24e892a4e519f8202c5cd24856f6e5b3d69
Scott Preston <spreston@icct.com>
2015-03-10 16:08:22 -0400

    updating git

--- js/test.js
+++ js/test.js
@@ -3,7 +3,7 @@ var testWidgets = document.querySelectorAll(".testWidget");
 
 for (var i = 0; i< testWidgets.length; i++) {
 	var div = testWidgets[i];
-	div.innerHTML = 'this is a test ' + new Date();
+	div.innerHTML = div.innerHTML  + ' ::this is a test ::' + new Date();
 }
 // testWidgets.forEach(function (elt) {
 // 	console.log(elt);

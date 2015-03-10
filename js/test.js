4f2fc74de33c9fa47c310d02fdcac3dcadebe247
Scott Preston <spreston@icct.com>
2015-03-10 15:51:29 -0400

    updating git

--- js/test.js
+++ js/test.js
@@ -1,3 +1,3 @@
 console.log('this is a test and it is working');
 var testWidget = document.querySelector("#testWidget");
-testWidget.innerHTML('this is a test ' + new Date());
+testWidget.innerHTML = 'this is a test ' + new Date();

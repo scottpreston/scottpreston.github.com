c731f141b9e59ddc2c4ed0192c66c022cf0ea8fc
Scott Preston <spreston@icct.com>
2015-03-24 16:51:39 -0400

    updating git

--- js/test2.js
+++ js/test2.js
@@ -1,1 +1,18 @@
-alert('hi jason');
+setTimeout(function() {
+
+    var hook = document.querySelector("#testscript");
+    var tableNode = hook.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
+    var table = document.querySelector("table");
+    var trs = document.querySelectorAll("tr");
+
+    console.log(trs)
+    for (var i = 0; i < trs.length; i++) {
+        var tds = trs[i].children;
+        for (var j = 0; j < tds.length; j++) {
+            var td = tds[j];
+            console.log(td.innerText);
+        }
+
+    }
+
+}, 1000);
